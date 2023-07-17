21699f8e67d286c2005ec41a43c0842280bd040e
i-prince <molatonny@gmail.com>
2023-07-17 13:51:42 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+  const hqLocation = 42;
+  return Math.abs(pickupLocation - hqLocation);
+}
+
+function distanceFromHqInFeet(pickupLocation) {
+  const blocks = distanceFromHqInBlocks(pickupLocation);
+  const feetPerBlock = 264;
+  return blocks * feetPerBlock;
+}
+
+function distanceTravelledInFeet(startBlock, endBlock) {
+  const blocks = Math.abs(endBlock - startBlock);
+  const feetPerBlock = 264;
+  return blocks * feetPerBlock;
+}
+
+function calculatesFarePrice(startBlock, endBlock) {
+  const distance = distanceTravelledInFeet(startBlock, endBlock);
+
+  if (distance <= 400) {
+    return 0;
+  } else if (distance > 400 && distance <= 2000) {
+    return (distance - 400) * 0.02;
+  } else if (distance > 2000 && distance < 2500) {
+    return 25;
+  } else if (distance >= 2500) {
+    return 'cannot travel that far';
+  }
+}
+
+console.log(distanceFromHqInBlocks(50));
+console.log(distanceFromHqInFeet(50));
+console.log(distanceTravelledInFeet(50,34));
+console.log(calculatesFarePrice(50,44));
--- test/indexTest.js
+++ test/indexTest.js
@@ -29,15 +29,15 @@ describe('index.js', function() {
 
   describe('distanceTravelledInFeet()', function() {
     it('returns the distance travelled in feet', function() {
-      expect(distanceTravelledInFeet(43, 48)).to.equal(1320);
+      expect(distanceTraveledInFeet(43, 48)).to.equal(1320);
     });
 
     it('returns a distance in feet', function() {
-      expect(distanceTravelledInFeet(50, 60)).to.equal(2640);
+      expect(distanceTraveledInFeet(50, 60)).to.equal(2640);
     });
 
     it('returns distance when destination is below distance', function() {
-      expect(distanceTravelledInFeet(34, 28)).to.equal(1584);
+      expect(distanceTraveledInFeet(34, 28)).to.equal(1584);
     });
   });
 
